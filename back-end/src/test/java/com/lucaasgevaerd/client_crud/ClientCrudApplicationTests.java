cea0d5502b4ef7dde2e28aee93e4e543605b933d
lucaasgevaerd <lucaasgevaerd@gmail.com>
2024-09-16 19:29:28 -0300

    first commit

---
+++ back-end/src/test/java/com/lucaasgevaerd/client_crud/ClientCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaasgevaerd.client_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
